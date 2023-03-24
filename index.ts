70820d1302a9092a81d25c3d77f580c2b48e0a4a
Akhil Kurnool <akhil1996kc@gmail.com>
2023-03-25 00:50:41 +0530

    chore: added .env with test APIs

--- index.ts
+++ index.ts
@@ -5,9 +5,34 @@ dotenv.config();
 
 const port = process.env.PORT;
 
+const sequelize = require('./database');
+const Templates = require('./models/Templates');
+
+sequelize.sync().then(() => {
+  console.log('DB is ready')
+});
+
 const app: Express = express();
 
 app.use(express.json());
+
+app.post('/templates', (req: Request, res: Response) => {
+  Templates.create(req.body).then((data) => {
+    console.log('New template created!');
+    res.send(data.toJSON());
+  })
+});
+
+app.get('/templates', (req: Request, res: Response) => {
+  Templates.findAll().then((data) => {
+    console.log('data? ', data)
+    res.send(data.toJSON());
+  });
+});
+
+app.delete('/templates', (req: Request, res: Response) => {
+
+});
 
 app.listen(port, () => {
   console.log('Im running');
