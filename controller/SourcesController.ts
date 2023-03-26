ef83fc2326acd5675878702d60264356b57455a2
Akhil Kurnool <akhil1996kc@gmail.com>
2023-03-26 18:24:12 +0530

    chore: added controller for source

--- controller/SourcesController.ts
+++ controller/SourcesController.ts
@@ -1,8 +1,73 @@
-import { Sources } from '../models/Sources';
+import { Op } from 'sequelize';
 
-export const CreateSource = async (reqBody) => {
-  const data = await Sources.create(reqBody);
-  const res = data.toJSON();
-  console.log(`New Data Source created with id: ${res.id}`);
-  return res;
+import { SourceFieldValues } from '../models/SourceFieldValues';
+import { Fields, TemplatesFieldsAssoc, Sources } from '../models';
+import { validateAllRequiredFields } from '../validators';
+
+interface ReqBodyField {
+  id: number,
+  value: string
+}
+
+export const CreateSource = async (templateId: number, name: string, reqfields: ReqBodyField[]) => {
+  if (!reqfields || reqfields.length === 0) {
+    throw new Error('fields key cannot be empty');
+  }
+  if (!name.trim()) {
+    throw new Error('name cannot be empty');
+  }
+  const templateRow = await Fields.findByPk(templateId);
+  if (templateRow ===  null) {
+    throw new Error(`Invalid templateId: ${templateId}`);
+  }
+  const TempateFieldsAssoc = await TemplatesFieldsAssoc.findAll({
+    where: {
+      templateId
+    },
+  });
+  if (TempateFieldsAssoc.length === 0) {
+    throw new Error(`No fields associated with templateId: ${templateId}`)
+  }
+  const fieldIds = TempateFieldsAssoc.map((tfa) => tfa.toJSON().fieldId);
+  const fieldsObj = await Fields.findAll({
+    where: {
+      id: {
+        [Op.in]: fieldIds
+      }
+    }
+  });
+  const fields = fieldsObj.map(f => f.toJSON());
+  const [isValid, reason] = validateAllRequiredFields(fields, reqfields);
+  if (!isValid) {
+    throw new Error(reason as string)
+  }
+  const data = await Sources.create({ name: name.trim() });
+  const id = data.toJSON().id;
+  return SourceFieldValues.bulkCreate(reqfields.map(f => ({ 
+    sourceId: id,  
+    templateId, 
+    fieldId: f.id, 
+    value: f.value 
+  })))
+}
+
+export const GetSource = async (sourceId: number) => {
+  const source = await Sources.findByPk(sourceId);
+  if (source === null) {
+    throw new Error(`No source with sourceId: ${sourceId}`);
+  }
+  const sourceFieldValues = await SourceFieldValues.findAll({
+    where: {
+      sourceId
+    }
+  })
+  if (sourceFieldValues.length === 0) {
+    throw new Error('No values setup');
+  }
+  const values = sourceFieldValues.map((f) => f.toJSON());
+  return {
+    id: sourceId,
+    name: source.toJSON().name,
+    field: values.map((f) => ({ id: f.id, value: f.value }))
+  }
 }
