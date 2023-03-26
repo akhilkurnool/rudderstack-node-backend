1d7177facccddf5f0e7ae717732c1e3d7338a6ab
Akhil Kurnool <akhil1996kc@gmail.com>
2023-03-26 19:15:27 +0530

    fix: getting host from .env

--- database.ts
+++ database.ts
@@ -3,8 +3,9 @@ import { Sequelize } from 'sequelize';
 const db = process.env.DB_NAME!;
 const user = process.env.DB_USERNAME!;
 const password = process.env.DB_PASS!;
+const host = process.env.HOST!;
 
 export const sequelize = new Sequelize(db, user, password, {
   dialect: 'sqlite',
-  host: './db.sqlite'
+  host
 });
