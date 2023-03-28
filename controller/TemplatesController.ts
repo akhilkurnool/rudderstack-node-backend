2cbe17a1b9530b48a054480382990af76ae60549
Akhil Kurnool <akhil1996kc@gmail.com>
2023-03-28 14:53:42 +0530

    fix: adding field id to association table

--- controller/TemplatesController.ts
+++ controller/TemplatesController.ts
@@ -9,17 +9,24 @@ interface TemplatesResult {
   fieldName: string,
   input_type: string,
   label: string,
+  required: number;
   regexErrorMessage: string | null,
   placeholder: string | null,
   regex: string | null,
   options: string | null
 }
 
-export const CreateNewTemplate = async (name: string) => {
+export const CreateNewTemplate = async (name: string, fieldIds?: Fields[]) => {
   const data = await Templates.create({ name })
   const res = data.toJSON();
   console.log(`New template created with id: ${res.id}`);
-  return res;
+  if (!fieldIds || fieldIds.length === 0) return res;
+  await TemplatesFieldsAssoc.bulkCreate(fieldIds.map((fieldId) => ({ templateId: res.id, fieldId })));
+  return {
+    id: res.id,
+    name,
+    fields: fieldIds
+  }
 }
 
 export const AssociateFieldsToTemplate = async (templateId: number, fieldIds: number[]) => {
@@ -39,41 +46,34 @@ export const GetTemplateById = async (id: number) => {
   return data;
 }
 
-
 // improve this!!, sequlize joins are not workding as expected, using raw query for now
 export const GetAllTemplates = async () => {
   const [res, _] = await sequelize.query(
-    `select t.id as templateId, t.name as templateName, f.id as fieldId, f.name as fieldName, f.input_type, f.label, f.regexErrorMessage, f.placeholder, f.regex, f.options from templates t
+    `select t.id as templateId, t.name as templateName, f.id as fieldId, f.name as fieldName, 
+      f.input_type, f.label, f.regexErrorMessage, f.placeholder, f.required, f.regex, f.options from templates t
       join template_field_assoc tfa on tfa.templateId = t.id
       join fields f on tfa.fieldId = f.id`);
-  console.log('a ', res);
   const obj = {} as { [key:string]: any };
   (res as TemplatesResult[]).forEach((template) => {
     if (obj[template.templateName] === undefined) {
       obj[template.templateName] = {
         id: template.templateId,
         name: template.templateName,
-        fields: [{
-          id: template.fieldId,
-          name: template.fieldName,
-          label: template.label,
-          regexErrorMessage: template.regexErrorMessage || undefined,
-          placeholder: template.placeholder || undefined,
-          regex: template.regex || undefined,
-          options: template.options ? JSON.parse(template.options) : undefined
-        }]
+        fields: []
       }
-    } else {
-      obj[template.templateName].fields.push({
-        id: template.fieldId,
-          name: template.fieldName,
-          label: template.label,
-          regexErrorMessage: template.regexErrorMessage || undefined,
-          placeholder: template.placeholder || undefined,
-          regex: template.regex || undefined,
-          options: template.options ? JSON.parse(template.options) : undefined
-      })
     }
+    console.log('template? ', template);
+    obj[template.templateName].fields.push({
+      id: template.fieldId,
+      name: template.fieldName,
+      label: template.label,
+      input_type: template.input_type,
+      regexErrorMessage: template.regexErrorMessage || undefined,
+      placeholder: template.placeholder || undefined,
+      regex: template.regex || undefined,
+      required: template.required === 1 ? true : false,
+      options: template.options ? JSON.parse(template.options) : undefined
+    })
   })
   return Object.values(obj);
 }
